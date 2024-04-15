42018bd483bc336aeaec87be6a8eeafcbd616a85
alex85amy <alex85amy@gmail.com>
2024-04-15 14:29:45 +0800

    update

--- src/com/company/DBCRUD.java
+++ src/com/company/DBCRUD.java
@@ -2,63 +2,39 @@ package com.company;
 
 
 import com.company.bean.ChannelInfo;
-import jakarta.json.*;
+import com.company.daoimpl.ChannelInfoDaoImpl;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
 
 public class DBCRUD {
-    public static void main(String[] args) {
-        try (JsonReader reader = Json.createReader(new FileReader("C:\\\\CRUD\\\\src\\\\com\\\\company\\\\channel_info.json"))) {
-            JsonArray jsonArray = reader.readArray();
+    public static void main(String[] args) throws FileNotFoundException {
+        // 讀取 JSON 文件
+        FileReader reader = new FileReader("C:\\CRUD\\src\\com\\company\\channel_info.json");
 
-            for (int i = 0; i < jsonArray.size(); i++) {
-                JsonObject jsonObject = jsonArray.getJsonObject(i);
+        Gson gson = new Gson();
+        Type mapType = new TypeToken<Map<String, List<ChannelInfo>>>() {
+        }.getType();
+        Map<String, List<ChannelInfo>> jsonMap = gson.fromJson(reader, mapType);
 
-                // 從每個 JSON 物件中擷取需要的資料
-                String sourceId = jsonObject.getString("source_id");
-                String sourceAreaId = jsonObject.getString("source_area_id");
-                int isUsed = jsonObject.getInt("is_used");
-                String pType2 = jsonObject.getString("p_type_2");
+        List<ChannelInfo> channelInfoList = jsonMap.get("channel_info");
 
-                // 建立 Java 物件
-                ChannelInfo channelInfo = new ChannelInfo(sourceId, sourceAreaId, isUsed, pType2);
-
-                // 使用 Java 物件
-                System.out.println("sourceId: " + channelInfo.getSourceId());
-                System.out.println("sourceAreaId: " + channelInfo.getSourceAreaId());
-                System.out.println("isUsed: " + channelInfo.getIsUsed());
-                System.out.println("pType2: " + channelInfo.getPType2());
-                System.out.println();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        ChannelInfoDaoImpl channelInfoDao = new ChannelInfoDaoImpl();
+        for (ChannelInfo channelInfo : channelInfoList) {
+            channelInfoDao.add(channelInfo);
         }
 
-
-//        try {
-//            // 创建 Gson 实例
-//            Gson gson = new Gson();
-//
-//            // 读取 JSON 文件到对象
-//            FileReader reader = new FileReader("C:\\CRUD\\src\\com\\company\\channel_info.json");
-//            List<ChannelInfo> channelInfos = gson.fromJson(reader, List.class);
-//
-//            // 将对象数组转换为列表
-//            List<ChannelInfo> channelInfoList = new ArrayList<>();
-//            for (ChannelInfo channelInfo: channelInfos){
-//                channelInfoList.add(channelInfo);
-//            }
-//
-//            // 打印每个人的信息
-//            for(ChannelInfo channelInfo : channelInfos) {
-//                System.out.println("sourceId: " + channelInfo.getSourceId());
-//                System.out.println("sourceAreaId: " + channelInfo.getSourceAreaId());
-//                System.out.println("isUsed: " + channelInfo.getIsUsed());
-//                System.out.println("pType2: " + channelInfo.getPType2());
-//                System.out.println();
-//            }
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
+//        for (ChannelInfo channelInfo : channelInfoList) {
+//                System.out.println("Source ID: " + channelInfo.getSourceId());
+//                System.out.println("Source Area ID: " + channelInfo.getSourceAreaId());
+//                System.out.println("Is Used: " + channelInfo.getIsUsed());
+//                System.out.println("P Type 2: " + channelInfo.getPType2());
+//            System.out.println();
 //        }
     }
 }
