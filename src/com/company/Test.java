bbae875559122a7d819a838ee5160527d8671b91
alex85amy <alex85amy@gmail.com>
2024-05-10 16:32:45 +0800

    update

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -5,10 +5,10 @@ import com.company.service.JsonImportService;
 
 public class Test {
     public static void main(String[] args) {
-//        JsonExportService jsonExport = new JsonExportService();
-//        jsonExport.exportTagInfo("tag_info0510.json");
-        JsonImportService jsonImport = new JsonImportService();
-        jsonImport.importTagInfo("tag_info0510.json");
+        JsonExportService jsonExport = new JsonExportService();
+        jsonExport.exportTagInfo("tag_info0510.json");
+//        JsonImportService jsonImport = new JsonImportService();
+//        jsonImport.importTagInfo("tag_info0510.json");
     }
 }
 
--- src/com/company/service/JsonExportService.java
+++ src/com/company/service/JsonExportService.java
@@ -38,13 +38,13 @@ public class JsonExportService {
         // 創建存放 ChannelInfo 的 JsonArray
         JsonArray jsonArray = new JsonArray();
         for (ChannelInfo channelInfo : list) {
-            JsonObject tagObject = new JsonObject();
-            tagObject.addProperty("auto_id", channelInfo.getAutoId());
-            tagObject.addProperty("source_id", channelInfo.getSourceId());
-            tagObject.addProperty("source_area_id", channelInfo.getSourceAreaId());
-            tagObject.addProperty("is_used", channelInfo.getIsUsed());
-            tagObject.addProperty("p_type_2", channelInfo.getPType2());
-            jsonArray.add(tagObject);
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("auto_id", channelInfo.getAutoId());
+            jsonObject.addProperty("source_id", channelInfo.getSourceId());
+            jsonObject.addProperty("source_area_id", channelInfo.getSourceAreaId());
+            jsonObject.addProperty("is_used", channelInfo.getIsUsed());
+            jsonObject.addProperty("p_type_2", channelInfo.getPType2());
+            jsonArray.add(jsonObject);
         }
 
         // 將 ChannelInfo 數組添加到根 JsonObject 中
@@ -68,23 +68,19 @@ public class JsonExportService {
         ChannelTagMappingDao channelTagMappingDao = new ChannelTagMappingDao(conn);
         List<ChannelTagMapping> list = channelTagMappingDao.findAll();
 
-        // 創建根 JsonObject
         JsonObject rootObject = new JsonObject();
 
-        // 創建存放 ChannelTagMapping 的 JsonArray
         JsonArray jsonArray = new JsonArray();
         for (ChannelTagMapping channelTagMapping : list) {
-            JsonObject tagObject = new JsonObject();
-            tagObject.addProperty("auto_id", channelTagMapping.getAutoId());
-            tagObject.addProperty("s_area_id", channelTagMapping.getSourceAreaId());
-            tagObject.addProperty("tag_id", channelTagMapping.getTagId());
-            jsonArray.add(tagObject);
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("auto_id", channelTagMapping.getAutoId());
+            jsonObject.addProperty("s_area_id", channelTagMapping.getSourceAreaId());
+            jsonObject.addProperty("tag_id", channelTagMapping.getTagId());
+            jsonArray.add(jsonObject);
         }
 
-        // 將 ChannelTagMapping 數組添加到根 JsonObject 中
         rootObject.add("channel_tag_mapping", jsonArray);
 
-        // 初始化 Gson 時設置格式化選項
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         try (FileWriter fileWriter = new FileWriter(fileName)) {
             gson.toJson(rootObject, fileWriter);
@@ -102,23 +98,19 @@ public class JsonExportService {
         PType2InfoDao pType2InfoDao = new PType2InfoDao(conn);
         List<PType2Info> list = pType2InfoDao.findAll();
 
-        // 創建根 JsonObject
         JsonObject rootObject = new JsonObject();
 
-        // 創建存放 PType2Info 的 JsonArray
         JsonArray jsonArray = new JsonArray();
         for (PType2Info pType2Info : list) {
-            JsonObject tagObject = new JsonObject();
-            tagObject.addProperty("auto_id", pType2Info.getAutoId());
-            tagObject.addProperty("category", pType2Info.getCategory());
-            tagObject.addProperty("name", pType2Info.getName());
-            jsonArray.add(tagObject);
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("auto_id", pType2Info.getAutoId());
+            jsonObject.addProperty("category", pType2Info.getCategory());
+            jsonObject.addProperty("name", pType2Info.getName());
+            jsonArray.add(jsonObject);
         }
 
-        // 將 PType2Info 數組添加到根 JsonObject 中
         rootObject.add("p_type_2_info", jsonArray);
 
-        // 初始化 Gson 時設置格式化選項
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         try (FileWriter fileWriter = new FileWriter(fileName)) {
             gson.toJson(rootObject, fileWriter);
@@ -132,26 +124,23 @@ public class JsonExportService {
     }
 
     public void exportTagInfo(String fileName) {
+
         TagInfoDao tagInfoDao = new TagInfoDao(conn);
         List<TagInfo> list = tagInfoDao.findAll();
 
-        // 創建根 JsonObject
         JsonObject rootObject = new JsonObject();
 
-        // 創建存放 TagInfo 的 JsonArray
         JsonArray jsonArray = new JsonArray();
         for (TagInfo tagInfo : list) {
-            JsonObject tagObject = new JsonObject();
-            tagObject.addProperty("tag_id", tagInfo.getTagId());
-            tagObject.addProperty("tag_name", tagInfo.getTagName());
-            tagObject.addProperty("type", tagInfo.getType());
-            jsonArray.add(tagObject);
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("tag_id", tagInfo.getTagId());
+            jsonObject.addProperty("tag_name", tagInfo.getTagName());
+            jsonObject.addProperty("type", tagInfo.getType());
+            jsonArray.add(jsonObject);
         }
 
-        // 將 TagInfo 數組添加到根 JsonObject 中
         rootObject.add("tag_info", jsonArray);
 
-        // 初始化 Gson 時設置格式化選項
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         try (FileWriter fileWriter = new FileWriter(fileName)) {
             gson.toJson(rootObject, fileWriter);
